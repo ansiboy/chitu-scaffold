91a30575b2abfec5062afd35414867d92fad3f16
mai.shu <ansiboy@163.com>
2021-04-12 11:12:48 +0800

    update

--- static/main.tsx
+++ static/main.tsx
@@ -1,21 +1,22 @@
 /// <reference path="./declare.d.ts" />
 
+let node_modules = `/node_modules`;
 var requirejsConfig = {
     "paths": {
-        "css": "node_modules/maishu-requirejs-plugins/src/css",
-        "text": "node_modules/maishu-requirejs-plugins/lib/text",
-        "json": "node_modules/maishu-requirejs-plugins/src/json",
+        "css": `${node_modules}/maishu-requirejs-plugins/src/css`,
+        "text": `${node_modules}/maishu-requirejs-plugins/lib/text`,
+        "json": `${node_modules}/maishu-requirejs-plugins/src/json`,
 
-        "react": "node_modules/react/umd/react.development",
-        "react-dom": "node_modules/react-dom/umd/react-dom.development",
-        "maishu-chitu": "node_modules/maishu-chitu/dist/index",
-        "maishu-chitu-react": "node_modules/maishu-chitu-react/dist/index.min",
-        "maishu-chitu-service": "node_modules/maishu-chitu-service/dist/index.min",
-        "maishu-dilu": "node_modules/maishu-dilu/dist/index.min",
-        "maishu-toolkit": "node_modules/maishu-toolkit/dist/index.min",
-        "maishu-ui-toolkit": "node_modules/maishu-ui-toolkit/dist/index.min",
-        "maishu-wuzhui": "node_modules/maishu-wuzhui/dist/index.min",
-        "maishu-wuzhui-helper": "node_modules/maishu-wuzhui-helper/dist/index.min",
+        "react": `${node_modules}/react/umd/react.development`,
+        "react-dom": `${node_modules}/react-dom/umd/react-dom.development`,
+        "maishu-chitu": `${node_modules}/maishu-chitu/dist/index`,
+        "maishu-chitu-react": `${node_modules}/maishu-chitu-react/dist/index.min`,
+        "maishu-chitu-service": `${node_modules}/maishu-chitu-service/dist/index.min`,
+        "maishu-dilu": `${node_modules}/maishu-dilu/dist/index.min`,
+        "maishu-toolkit": `${node_modules}/maishu-toolkit/dist/index.min`,
+        "maishu-ui-toolkit": `${node_modules}/maishu-ui-toolkit/dist/index.min`,
+        "maishu-wuzhui": `${node_modules}/maishu-wuzhui/dist/index.min`,
+        "maishu-wuzhui-helper": `${node_modules}/maishu-wuzhui-helper/dist/index.min`,
     }
 }
 
