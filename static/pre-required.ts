6c98b44af79a6405afdb80a876ed1d4e70d2762d
mai.shu <ansiboy@163.com>
2021-05-12 11:05:37 +0800

    update

--- static/pre-required.ts
+++ static/pre-required.ts
@@ -1,5 +1,5 @@
-exports["react"] = require("react");
-exports["react-dom"] = require("react-dom");
+// exports["react"] = require("react");
+// exports["react-dom"] = require("react-dom");
 exports["maishu-chitu"] = require("maishu-chitu");
 exports["maishu-chitu-react"] = require("maishu-chitu-react");
 exports["maishu-chitu-service"] = require("maishu-chitu-service");
